00c578c92523afd1de910ea6414bd47182638003
hamedfathi <hamedfathi@outlook.com>
2021-11-13 20:03:43 +0330

    feat: functionalities added.

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+// rollup.config.js
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+    input: "./src/index.ts",
+    output: [{
+        format: "iife",
+        file: "./dist/index.iife.js"
+    },
+    {
+        file: './dist/index.js',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true
+    },
+    {
+        file: './dist/index.es.mjs',
+        format: 'es',
+        exports: 'named',
+        sourcemap: true
+    },
+    {
+        file: './dist/index.umd.js',
+        format: 'umd',
+        name: '--name-umd--',
+        sourcemap: true
+    }],
+    plugins: [
+        typescript({
+            tsconfig: "./tsconfig.json"
+        })
+    ]
+};
