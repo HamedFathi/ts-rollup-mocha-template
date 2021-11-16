403342321c441b15b5c476310e8658813f729033
hamedfathi <hamedfathi@outlook.com>
2021-11-16 14:34:16 +0330

    feat: eslint & prettier added.

--- test/hello.spec.ts
+++ test/hello.spec.ts
@@ -3,10 +3,8 @@ import { expect } from 'chai';
 import 'mocha';
 
 describe('Hello function', () => {
-
-  it('should return hello world', () => {
-    const result = hello();
-    expect(result).to.equal('Hello world!');
-  });
-
+    it('should return hello world', () => {
+        const result = hello();
+        expect(result).to.equal('Hello world!');
+    });
 });
